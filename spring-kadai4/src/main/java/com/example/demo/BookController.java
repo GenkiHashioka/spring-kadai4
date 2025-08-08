8b1aa78c5e74f2c12b3d2c0ddc27a8f820442fc3
genkichi <genshigoto0821@gmail.com>
2025-08-08 12:41:01 +0900

    step2 完成

--- spring-kadai4/src/main/java/com/example/demo/BookController.java
+++ spring-kadai4/src/main/java/com/example/demo/BookController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -25,5 +27,27 @@ public class BookController {
 		mv.setViewName("book_search");
 		return mv;
 	}
-
+	
+//	検索用のGETメソッドを作成する
+	@RequestMapping("/search")
+//	書籍コードが未入力の場合でもページが表示できるようにrequired = falseをつける
+	public ModelAndView searchBooks(@RequestParam(name = "code", required = false) String codeStr, ModelAndView mv) {
+//		bookListに書籍一覧を取得する.
+		List<Book> bookList = new ArrayList<Book>();
+		// 書籍コードが未入力の場合は全件取得する
+		if (codeStr == null || codeStr.trim().isEmpty()) {
+			bookList = bookRepository.findAll();
+//			入力が数字であった場合に一致検索
+		} else if (codeStr.chars().allMatch(Character::isDigit)) {
+			bookList = bookRepository.findByCodeEquals(Long.parseLong(codeStr));
+		}
+		// 取得したリストの中身がなかった場合のエラーメッセージ
+		if (bookList.isEmpty()) {
+			mv.addObject("message", "該当するデータはありません。");
+		}
+		
+		mv.addObject("books", bookList);
+		mv.setViewName("book_search");
+		return mv;
+	}
 }
--- spring-kadai4/src/main/java/com/example/demo/BookRepository.java
+++ spring-kadai4/src/main/java/com/example/demo/BookRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //インターフェースの作成JpaRepositoryを継承　データの参照に使用する。
 public interface BookRepository extends JpaRepository<Book, Long> {
-	
+	List<Book> findByCodeEquals(Long code);
 }
